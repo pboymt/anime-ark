7ec988cfdfec935c473ca5ad0d53ceee8339c04c
pboymt <pboymt@foxmail.com>
2016-08-02 14:45:26 +0800

    db

--- save-to-db.class.ts
+++ save-to-db.class.ts
@@ -1,6 +1,6 @@
 /// <reference path="typings/index.d.ts" />
 import * as mongoose from "mongoose";
-mongoose.connect('mongodb://localhost/selflytv');
+mongoose.connect('mongodb://localhost/ark');
 var db = mongoose.connection;
 db.once('open', () => {
     console.log("DB OK");
