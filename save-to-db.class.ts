a5049c8220c8154c28426caa37dd761dbd86459e
pboymt <pboymt@foxmail.com>
2016-08-02 18:59:52 +0800

    schema

--- save-to-db.class.ts
+++ save-to-db.class.ts
@@ -2,6 +2,28 @@
 import * as mongoose from "mongoose";
 mongoose.connect('mongodb://localhost/ark');
 var db = mongoose.connection;
+var _schema: mongoose.Schema = new mongoose.Schema({
+    hash: {
+        type: String,
+        require: true
+    },
+    title: {
+        type: String,
+        require: true
+    },
+    from: {
+        type: String,
+        require: true
+    },
+    timestamp: {
+        type: Number,
+        require: true
+    },
+    torrent: {
+        type: String,
+        require: true
+    }
+});
 db.once('open', () => {
     console.log("DB OK");
 });
